ff969ae8dc227970d4b68cf9d4014019775e7976
Honza Tichavský <jan.tichavsky@netrex.com>
2021-04-29 13:35:36 +0200

    init

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+    "https://www.youtube.com/watch?v=6tHEWDnxj2Y",
+    "https://www.youtube.com/watch?v=d6bWbpQ2Hts",
+    "https://www.youtube.com/watch?v=YHUQpm2xv-o",
+    "https://www.youtube.com/watch?v=zXpV8pbrrAo",
+    "https://www.youtube.com/watch?v=bHLJNI6AGMs",
+    "https://www.youtube.com/watch?v=0_Pc44odF4g",
+    "https://www.youtube.com/watch?v=LXWVYoBluT4"
+];
